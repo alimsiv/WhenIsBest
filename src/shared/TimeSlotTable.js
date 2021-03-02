25bc5bcee0a05830eb8b446d8eebe8afb90e0c05
Ali Sivilotti <ali@sivilotti.com>
2021-03-02 15:11:05 -0500

    Comments added for TimeSlotTable

--- src/shared/TimeSlotTable.js
+++ src/shared/TimeSlotTable.js
@@ -1,6 +1,11 @@
 import '../styling/TimeSlotTable.css';
-import {forEach} from "react-bootstrap/ElementChildren";
 
+/**
+ * Adds each table header to the top row of TimeSlotTable is the dates/days
+ * @param date
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const AddHeaderDate = ({date}) => {
     const options = { weekday: 'long', month: 'long', day: 'numeric' };
     const dateString = date.toLocaleDateString(undefined, options).split(',') //Uses local OS language
@@ -19,16 +24,35 @@ const handleTimeSlotClicked = (time) => {
     console.log("Clicked: " + time);
 }
 
-
+/**
+ * Creates a tr (a table row) of the TimeSlotTable
+ * @param timestamp Integer corresponding to the timestamp for that row. Minutes since midnight.
+ * @param showTimeSlot Boolean array for this row, denoting which data cells should be available for users to click on
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const TimeSlotRow = (timestamp, showTimeSlot) => {
     let title = "";
-    if (timestamp % 60 === 0){
-        //if timestamp is an hour mark, add title
-        title = timestamp/60;
-        title += (title < 12) ? ' AM' : ' PM';
+    let rowClassName;
+
+    switch(timestamp % 60) {
+        case 0:
+            //if timestamp is an hour mark, add title
+            title = timestamp / 60;
+            title += (title < 12) ? ' AM' : ' PM';
+            rowClassName = 'timeslotClickableHour';
+            break;
+        case 30:
+            //if timestamp is on a half hour mark, change className for styling purposes
+            rowClassName = 'timeslotClickableHalfHour';
+            break;
+        default:
+            rowClassName = 'timeslotClickable';
     }
 
-    //TODO: some sort of mapping to trap which times for which days should be shown. Use a boolean 2x2 array?
+    //Counts which day (column) the cell is on. Used for creating key for cells.
+    let dayCount = 0;
+
     return (
         <>
             <tr>
@@ -36,33 +60,36 @@ const TimeSlotRow = (timestamp, showTimeSlot) => {
                     {title}
                 </td>
                 {showTimeSlot.map(show =>
-                    {if(show){
-                        //if the admin choose for this time to be available to be selected, make it clickable,
-                        // otherwise mark as unavailable slot and do not attach any event handlers
-                        //TODO: add key
-                        return <td
-                                   className="timeslotClickable"
-                                   onClick={() => handleTimeSlotClicked(timestamp)}/>
-                    } else {
-                        return <td
-                                   className="timeslotUnavailable"/>
-                    }
-                    }
+                    {
+                        const keyName = 'timeslot:' + dayCount + ':' + timestamp;
+                        dayCount++;
 
-                    )}
-                {/*{dateTimes.forEach((date, times) => <td key={"date:" + time}
-                                       className="timeslotClickable"
-                                       onClick={() => handleTimeSlotClicked(time)}/>)}*/}
+                        if(show){
+                            //if the admin choose for this time to be available to be selected, make it clickable,
+                            // otherwise mark as unavailable slot and do not attach any event handlers
+                            return <td
+                                    key={keyName}
+                                    className={rowClassName}
+                                    onClick={() => handleTimeSlotClicked(timestamp)}/>
+                        }
+                        else {
+                            return <td
+                                    key={keyName}
+                                    className="timeslotUnavailable"/>
+                        }
+                    })}
             </tr>
-
-            {/*<tr className="timeslot">
-                <td/>
-                {cols.map(() => <td/>)}
-            </tr>*/}
         </>
     );
 }
 
+/**
+ * Creates the tbody of the TimeSlotTable. Goes through each 15-minute timeslot and creates the row for it
+ * @param showTimeSlot See TimeSlotTable
+ * @param minStartTime See TimeSlotTable
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const TimeSlotCreateRows = (showTimeSlot, minStartTime) => {
     let timestamp = minStartTime;
     const rows = [];
@@ -79,18 +106,15 @@ const TimeSlotCreateRows = (showTimeSlot, minStartTime) => {
 }
 
 
-/**
+/** Creates the TimeSlotTable
  *
  * @param dates Date objects for each date, which is translated into each column
- * @param times
  * @param showTimeSlot 2x2 Boolean array that is the total size of the table, with each boolean denoting whether or not that element is "available"
- * @param minStartTime The earliest time that the table should start, aka the first row
+ * @param minStartTime Integer. The earliest time that the table should start, aka the first row. Minutes since midnight.
  * @returns {JSX.Element}
  * @constructor
  */
-const TimeSlotTable = ({dates, times, showTimeSlot, minStartTime}) => {
-    // Total time range for min start time to max end time (placeholder below, need to write function)
-    const timeRange = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM"];
+const TimeSlotTable = ({dates, showTimeSlot, minStartTime}) => {
 
     return (
         <div className="TimeSlotTable">
@@ -102,8 +126,6 @@ const TimeSlotTable = ({dates, times, showTimeSlot, minStartTime}) => {
                     </tr>
                 </thead>
                 {TimeSlotCreateRows(showTimeSlot,minStartTime)}
-                    {/*{timeRange.map((time) => <TimeSlotRow time={time} dateTimes={times} showTimeSlot={showTimeSlot} minStartTime={minStartTime}/>)}*/}
-
             </table>
         </div>
     );
