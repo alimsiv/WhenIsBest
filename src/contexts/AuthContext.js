ce8c67fb158037a7bc91e885f1d8eb6d75795382
tomciardi <72100170+tomciardi@users.noreply.github.com>
2021-03-12 12:19:12 -0500

    sign up page user interface

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -12,7 +12,7 @@ export function AuthProvider({ children }) {
     const[loading, setLoading] = useState(true)
 
     function signup(email, password) {
-        return auth.creatUserWithEmailAndPassword(email, password)
+        return auth.createUserWithEmailAndPassword(email, password)
     }
 
     useEffect(() => {
