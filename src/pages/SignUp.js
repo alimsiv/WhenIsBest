cd5ae506bf27d1a8ab64c79051dbe4936492a973
tomciardi <72100170+tomciardi@users.noreply.github.com>
2021-03-12 12:19:12 -0500

    sign up page user interface

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { Alert, Button, Card, Form } from 'react-bootstrap'
+import { Alert, Button, Card, Container, Form } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 
 export default function SignUp() {
@@ -28,32 +28,36 @@ export default function SignUp() {
 
     return (
         <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <Form onSubmit={handleSubmit}>
-                        <Form.Group id="email">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" ref={emailRef} required />
-                        </Form.Group>
-                        <Form.Group id="password">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" ref={passwordRef} required />
-                        </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submit">
-                            Sign Up
-                        </Button>
-                    </Form>
-                </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2">
-                Already have an account? Log in
-            </div>
+            <Container className="d-flex align-tems-center justify-content-center" style={{ minHeight: "100vh" }}>
+                <div className="w-100" style={{ maxWidth: "400px" }}>
+                    <Card>
+                        <Card.Body>
+                            <h2 className="text-center mb-4">Sign Up</h2>
+                            {error && <Alert variant="danger">{error}</Alert>}
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group id="email">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" ref={emailRef} required />
+                                </Form.Group>
+                                <Form.Group id="password">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" ref={passwordRef} required />
+                                </Form.Group>
+                                <Form.Group id="password-confirm">
+                                    <Form.Label>Confirm Password</Form.Label>
+                                    <Form.Control type="password" ref={passwordConfirmRef} required />
+                                </Form.Group>
+                                <Button disabled={loading} className="w-100" type="submit">
+                                    Sign Up
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                    <div className="w-100 text-center mt-2">
+                        Already have an account? Log in
+                    </div>
+                </div>
+            </Container>
         </>
     )
 }
