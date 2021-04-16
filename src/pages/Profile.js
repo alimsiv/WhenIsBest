1cdcd4609612bb36ffe62b832ce57de633dde7b3
tomciardi <72100170+tomciardi@users.noreply.github.com>
2021-04-16 10:03:41 -0400

    updated ui

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -9,28 +9,32 @@ export default function Profile() {
 
     return (
         <>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Profile</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <strong>Email:</strong> {currentUser.email}
-                    <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-                        Update Profile
-                    </Link>
-                </Card.Body>
-            </Card>
-            <Card>
-                <Card.Body>
-                    <h2 className="text-center mb-4">Upcoming Meetings</h2>
-                    {error && <Alert variant="danger">{error}</Alert>}
-                    <strong>Email:</strong> {currentUser.email}
-                </Card.Body>
-            </Card>
-                 <div className="w-100 text-center mt-2">
-                <Button variant="link" onClick={() => handleLogout(setError, logout)}>
-                    Log Out
-                </Button>
-            </div>    
+            <Container className="d-flex align-tems-center justify-content-center" style={{ minHeight: "100vh" }}>
+                <div className="w-100" style={{ maxWidth: "800px" }}>
+                    <Card>
+                        <Card.Body>
+                            <h2 className="text-center mb-4">Profile</h2>
+                            {error && <Alert variant="danger">{error}</Alert>}
+                            <strong>Email:</strong> {currentUser.email}
+                            <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+                                Update Profile
+                            </Link>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <Card.Body>
+                            <h2 className="text-center mb-4">Upcoming Meetings</h2>
+                            {error && <Alert variant="danger">{error}</Alert>}
+                            <strong>Email:</strong> {currentUser.email}
+                        </Card.Body>
+                    </Card>
+                        <div className="w-100 text-center mt-2">
+                        <Button variant="link" onClick={() => handleLogout(setError, logout)}>
+                            Log Out
+                        </Button>
+                    </div>  
+                </div>
+            </Container>  
         </>
     )
 }
