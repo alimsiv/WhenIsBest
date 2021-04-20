776c5527bee779cb591790e6fbc05dccb2079f93
Ali Sivilotti <ali@sivilotti.com>
2021-04-20 00:37:36 -0400

    heatmap working

--- src/shared/TimeSlotTable.js
+++ src/shared/TimeSlotTable.js
@@ -75,6 +75,59 @@ class TimeSlotTable extends Component {
         return response;
     }
 
+    getRBG(scale){
+        
+        //For red to green gradient
+        let r = 255;
+        let g = 255;
+        let b = 0;
+        if (scale < 127){
+            g = scale*2;
+        }
+        else {
+            r = 255 - (scale-127)*2;
+        }
+        r = Math.round(r).toString(16).padStart(2,'0');
+        g = Math.round(g).toString(16).padStart(2,'0');
+        b = Math.round(b).toString(16).padStart(2,'0');
+
+        return "#" + r + "" + g + "" + b;
+
+        
+
+        /* for white to green gradient
+        let rb = Math.round(scale).toString(16).padStart(2,'0');
+
+        const rgb = "#" + rb + "ff" + rb;
+
+        return "#" + rb + "ff" + rb;
+        */
+
+        /*For white to red to green gradient
+        let r = 255;
+        let g = 255;
+        let b = 255;
+        if (scale < 85){
+            g = 255 - scale*3;
+            b = 255 - scale*3;
+        }
+        else if (scale < 170){
+            g = (scale-85)*3;
+            b = 0;
+        }
+        else {
+            r = 255 - (scale-170)*3;
+            b = 0;
+        }
+        r = Math.round(r).toString(16).padStart(2,'0');
+        g = Math.round(g).toString(16).padStart(2,'0');
+        b = Math.round(b).toString(16).padStart(2,'0');
+
+        return "#" + r + "" + g + "" + b;*/
+
+
+    }
+
     // Returns matrix location from cell id
     getMatrixLocation(id) {
         const parsedID = id.split(':'); //example: timeslot:0:540
@@ -88,15 +141,17 @@ class TimeSlotTable extends Component {
 //        const row = (parsedID[2] - this.props.minStartTime) / 15;
 //        return [row, parsedID[1]];
 
-        let row = timestamp - this.props.minStartTime;
+        let row = (timestamp - this.props.minStartTime)/15;
         const index = (row * this.props.tableCol) + dayCount;
 
         const colorM = this.props.colorMap;
         const colorInt = this.props.colorMap[index];
 
-        const colorHex = parseInt(colorInt, 16);
+        const colorHex = colorInt.toString(16);
+        const colorPad = String(colorHex).padStart(2, '0');
         //return colorHex;
-        return "#ffff" + colorHex;
+        return this.getRBG(colorInt);
+        return "#ffff" + colorPad;
         return "#000000";
     }
 
@@ -321,6 +376,7 @@ class TimeSlotTable extends Component {
                     {this.TimeSlotCreateRows()}
                 </table>
                 <br />
+                {!this.props.isInputTable && <div className="gradient-box"></div>}
             </div>
 
         );
