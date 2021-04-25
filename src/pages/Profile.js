073b105ec9f12aa9e04a2541b996f13bd719ecd9
tomciardi <72100170+tomciardi@users.noreply.github.com>
2021-04-25 14:08:06 -0400

    added meeting names to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,13 +2,83 @@ import React, { useState, useAsync } from 'react'
 import { Card, Button, Alert, Container, Nav } from 'react-bootstrap'
 import { useAuth, handleLogout } from '../contexts/AuthContext'
 import { Link } from 'react-router-dom'
-import { useDocument, useCollectionData } from 'react-firebase-hooks/firestore'
-import { firestore } from '../apis/firebase'
 import firebase from 'firebase/app';
+import { useDocument, useCollection } from 'react-firebase-hooks/firestore'
 
 export default function Profile() {
-    const [error, setError] = useState("");
-    const { currentUser, logout } = useAuth();
+    const [error, setError] = useState("")
+    const { currentUser, logout } = useAuth()
+    
+    const Meetings = () => {
+        let meetingsList = []
+        let meetings = [];
+
+        const [docValue] = useDocument(
+          firebase.firestore().doc(`users/${currentUser.uid}`),
+          {
+            snapshotListenOptions: { includeMetadataChanges: true },
+          }
+        );
+
+        const [collectValue] = useCollection(
+            firebase.firestore().collection('meetings'),
+          {
+            snapshotListenOptions: { includeMetadataChanges: true },
+          }
+        );
+
+        if (docValue != null) { 
+            meetingsList = docValue.data().meetings;
+            for (let i = 0; i < meetingsList.length; i++) {
+                const meeting = meetingsList[i]
+                if(collectValue != null) {
+                    const docArray = collectValue.docs
+                    for(let j = 0; j < docArray.length; j++) {
+                        let name = ''
+                        if(docArray[j].id == meeting.toString()) {
+                            name = docArray[j].data().name
+                            meetings.push(
+                                <Nav.Link key={meeting} href={"/view/" + meeting.toString()}>{name}</Nav.Link>
+                            )
+                        }
+                    }
+                }
+            }   
+        }
+        console.log(meetings)
+        return (
+          <div>
+            {meetings && <Nav className="flex-column">{ meetings }</Nav>}
+          </div>
+        );
+      };
+
+    /**
+    const Test = () => {
+        let meetings = []
+        let meetingLinks = []
+        firebase.firestore().collection('user').doc(`${currentUser.uid}`).onSnapshot((doc) => {
+            if(doc.exists) {
+                meetings = doc.data().meetings
+                console.log(meetings)
+                for (let i = 0; i < meetings.length; i++) {
+                    firebase.firestore().collection('meetings').doc(meetings[i].toString()).onSnapshot((doc) => {
+                        if(doc.exists) {
+                            meetingLinks.push(
+                                <Nav.Link key={meetings[i]} href={"/view/" + meetings[i].toString()}>{doc.data().name}</Nav.Link>)
+                        }
+                    });
+                }
+            }
+        });
+        console.log(meetingLinks)
+        return (
+            <div>
+                {meetingLinks && <Nav className="flex-column">{ meetingLinks }</Nav>}
+            </div>
+        );
+    };
+    */
 
     return (
         <>
@@ -28,6 +98,7 @@ export default function Profile() {
                         <Card.Body>
                             <h2 className="text-center mb-4">Upcoming Meetings</h2>
                             {error && <Alert variant="danger">{error}</Alert>}
+                            <Meetings/>
                         </Card.Body>
                     </Card>
                     <div className="w-100 text-center mt-2">
