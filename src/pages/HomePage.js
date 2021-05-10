985dada4399ffefd01f4ff9297aed32004ce527d
tomciardi <72100170+tomciardi@users.noreply.github.com>
2021-05-10 19:22:00 -0400

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -114,16 +114,8 @@ class HomePage extends Component{
             <div className="flex" style={{"background": "linear-gradient(to bottom, #ffffff 0%, #0099ff 100%)"}}>
 
               <div className="flex-child" style={{"float": "left", "margin-left": "85px"}}>
-                <img src={example} style={{"border": "5px solid", "margin-left": "10px", "margin-top": "50px", "margin-right": "10px", "margin-bottom": "10px"}}/>
-                <p>"WhenIsBest allows for the creation of groups and the ability
-                to weigh the importance of attendance for both individuals and groups.
-                Additionally, WhenIsBest allows individuals to indicate general availability
-                versus preferred availability. The app then embeds a prioritization feature
-                into its algorithm that considers the weighted values of individuals and groups
-                and each individual’s preferences when calculating the most ideal meeting time.
-                Meeting time options are visualized in a comprehensive yet easily filterable
-                heat map that allows for the identification of the best time."
-                </p>
+                <h3 style={{ "margin-top": "25px"}}>Don't find a good time. Find a best time.</h3>
+                <img src={example} style={{"border": "5px solid", "margin-left": "10px", "margin-top": "25px", "margin-right": "10px", "margin-bottom": "10px"}}/>
               </div>
               <>
                 <Container className="d-flex align-tems-center justify-content-center" style={{minHeight: "100vh", "margin-right": "75px", "margin-top": "50px" }}>
@@ -154,10 +146,10 @@ class HomePage extends Component{
                                           <Form.Label>Your Meetings</Form.Label>
                                       </Form.Group>
                                       <Form.Group id="your-meetings">
-                                        <Button id="create-meeting" style={{"marginRight": "5px"}} onClick={() => this.createMeeting()}>
+                                        <Button id="create-meeting" onClick={() => this.createMeeting()}>
                                           Create a Meeting
                                         </Button>
-                                        <Button id="view-meetings" onClick={() => this.viewMeetings()}>
+                                        <Button id="view-meetings" style={{"marginTop": "5px"}} onClick={() => this.viewMeetings()}>
                                           View My Meetings
                                         </Button>
                                       </Form.Group>
@@ -166,9 +158,6 @@ class HomePage extends Component{
                                 </Card>
                             </Card.Body>
                         </Card>
-                        <div className="w-100 text-center mt-2">
-                            Don't have an account? <Link to="/signup">Sign Up</Link>
-                        </div>
                     </div>
                 </Container>
               </>
